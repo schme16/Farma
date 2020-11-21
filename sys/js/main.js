4ef06d2da829cce0c471570466fbc93dad488570
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2020-11-21 15:38:35 +1000

    starting to fill out stubs

--- sys/js/main.js
+++ sys/js/main.js
@@ -1,20 +1,70 @@
+let m 
 angular.module('farma', [])
 
 /*The master controller*/
 .controller('master', ($scope, $sce, $compile) => {
-	let m = $scope
-	m.query = Qs.parse(location.search.substr(1))
+	m = $scope
 	
-	m.$root.page = m.query.page || 'menu'
+	//Mark the game as still loading
+	m.$root.loading = true
+
+	m.database = new PouchDB('saves')
+	
+	//Get the hash query as of page load.
+	m.query = Qs.parse(location.hash.replace('#!#', '').replace('#', ''))
+	
+	//Set the page as per the query, or failing that, just load the menu
+	m.$root.page = m.query.page || 'loading'
+
+	//Initialize the app
+	m.init = () => {
+		m.$root.loading = false
+		m.setPage('main-menu')
+	}
+	
+	//Handles all the guff related to transitioning the old page into the new one
+	m.setPage = (page, pre, mid, post) => {
+		if (!m.$root.transitioning) {
+			let pageEl = $('.page'),
+				duration = m.$root.page == page ? 0 : 600
+			
+			m.$root.transitioning = true
+			m.$applyAsync()
+			if (pre) pre()
+			
+			pageEl.velocity('transition.flipXOut', {duration: duration, complete: () => {
+				m.$root.page = page
+				m.$applyAsync()
+				if (mid) mid()
+				
+				pageEl.velocity('transition.flipXIn', {duration: duration, complete: () => {
+					m.$root.transitioning = false
+					m.$applyAsync()
+					if (post) post()
+				}})
+			}})
+		}
+	}
 	
 	//Creates a blank game state, used for new games, and while loading game states
 	m.newGameState = () => {
-		//TODO: Create the game state structure
+		m.game = {
+			state: {
+			
+			}
+		}
+		
 	}
 	
 	//Creates a new game
 	m.newGame = () => {
-		
+		m.newGameState()
+		m.setPage('game', null, ()=> {
+			requestAnimationFrame(() => {
+				m.initializeEngine()
+			})
+			
+		})
 	}
 	
 	//Save game state into a file
@@ -39,9 +89,10 @@ angular.module('farma', [])
 	
 	//Creates the game engine (Phaser currently)
 	m.initializeEngine = () => {
-		m.game = {}
 		m.game.config = {
 			type: Phaser.AUTO,
+			parent: $('<div>')[0],
+			autoCenter: Phaser.Scale.CENTER_BOTH,
 			width: 800,
 			height: 600,
 			scene: {
@@ -51,7 +102,10 @@ angular.module('farma', [])
 			}
 		}
 		
-		m.game.engine = new Phaser.Game(config)
+		m.game.engine = new Phaser.Game(m.game.config)
+		$(m.game.engine.renderer.canvas).appendTo('.game')
+
+		
 
 	}
 	
@@ -70,4 +124,23 @@ angular.module('farma', [])
 	
 	}
 	
+	
 })
+	
+/*Directives*/
+	
+/*Turns off the ng-scope, et al. debug classes*/
+.config(['$compileProvider', function ($compileProvider) {
+	$compileProvider.debugInfoEnabled(false);
+}])
+
+/*.directive('test', function () {
+	return {
+		restrict: 'E',
+		scope: true,
+		//templateUrl: '',
+		//transclude: true,
+		link: function (scope, element, attrs) {
+		}
+	}
+})*/
