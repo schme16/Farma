6187c0700211d37dae70bacb29524f8f85f08a14
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2020-11-24 15:45:52 +1000

    Grid works, bed builds, seeds plant, camera centers, looks like poops, but getting close to minimum viable product :)

--- sys/js/main.js
+++ sys/js/main.js
@@ -8,6 +8,7 @@ angular.module('farma', [])
 	//Mark the game as still loading
 	m.$root.loading = true
 
+	//Load the database
 	m.database = new PouchDB('saves')
 	
 	//Get the hash query as of page load.
@@ -15,7 +16,7 @@ angular.module('farma', [])
 	
 	//Set the page as per the query, or failing that, just load the menu
 	m.$root.page = m.query.page || 'loading'
-
+	
 	//Initialize the app
 	m.init = () => {
 		m.$root.loading = false
@@ -53,8 +54,9 @@ angular.module('farma', [])
 	//Creates a blank game state, used for new games, and while loading game states
 	m.newGameState = () => {
 		m.game = {
+			clearList:[],
 			state: {
-				gardenSize: 3,
+				gardenSize: 9,
 				plants: {}
 			}
 		}
@@ -63,13 +65,18 @@ angular.module('farma', [])
 	
 	//Creates a new game
 	m.newGame = () => {
-		m.newGameState()
-		m.setPage('game', null, ()=> {
-			requestAnimationFrame(() => {
-				m.initializeEngine()
+		if (!m.$root.transitioning) {
+			m.clearGame()
+
+			m.newGameState()
+
+			m.setPage('game', null, () => {
+				requestAnimationFrame(() => {
+					m.initializeEngine()
+				})
+
 			})
-			
-		})
+		}
 	}
 	
 	//Save game state into a file
@@ -93,14 +100,41 @@ angular.module('farma', [])
 			
 		//Truncate the filename at 32
 		filename = filename.substr(0,32)
-		console.log(filename)
+
+		//Fetch teh record
 		m.database.get(filename).then(function (response) {
+			//TODO: do error catching, currently assumes valid responses 
+			
+			//Clear the current game, if any
+			m.clearGame()
+			
+			//Create a basic state
 			m.newGameState()
+			
+			//Load the saved state
 			m.game.state = response.state
+			
+			//Start the engine
+			m.initializeEngine()
+			
 		}).catch(function (err) {
 			console.log(err);
 		})
 		
+	}
+	
+	//Destroys or otherwise resets all of the engine stuff.
+	m.clearGame = () => {
+		if (m.game && m.game.clearList) {
+			for (let i in m.game.clearList) {
+				try {
+					eval(`${m.game.clearList[i]}.destroy()`)
+					
+				} catch(e) {
+					console.log(1)
+				}
+			}
+		}
 	}
 	
 	//Used to force the game state to match the given data - used for loading games
@@ -122,59 +156,47 @@ angular.module('farma', [])
 		
 		if (m.game.layers.borders) m.game.layers.borders.destroy()
 		m.game.layers.borders = m.game.map.createBlankDynamicLayer('borders', m.game.tiles.borders)
-		
-		//Set the top left of the garden bed, and save that to the game object
-		m.game.state.gardenPos = m.game.map.worldToTileXY((m.game.config.width / 2) - (m.game.state.gardenSize / 2), (m.game.config.height / 2) - (m.game.state.gardenSize / 2))
-		m.game.state.gardenPos.x = m.game.state.gardenPos.x - 1
-		m.game.state.gardenPos.y = m.game.state.gardenPos.y - 1
-		let maxPosX = m.game.state.gardenPos.x + m.game.state.gardenSize,
-			maxPosY = m.game.state.gardenPos.y + m.game.state.gardenSize
-		
-		
-		
-		
-		
-		m.game.validCells = m.returnValidFarmCells(m.game.state.gardenPos.x, m.game.state.gardenPos.y, maxPosX, maxPosY)
-		
-		for (let y = m.game.state.gardenPos.y; y < maxPosY; y++) {
-			for (let x = m.game.state.gardenPos.x; x < maxPosX; x++) {
+			
 				
+		for (let y = 0; y < m.game.state.gardenSize; y++) {
+			for (let x = 0; x < m.game.state.gardenSize; x++) {
 
-				
-				
-				//Add the top left corner grid tile
 				m.game.layers.grid.putTileAt(9, x, y)
 
 				//Handle the corners
-				
+			
 				//Top left corner
-				if (y == m.game.state.gardenPos.y && x == m.game.state.gardenPos.x) {
+				if (y == 0 && x == 0) {
 		            m.game.layers.borders.putTileAt(0, x, y)
 				}
 				
 				//Top right corner
-				else if (y == m.game.state.gardenPos.y && x == maxPosX-1) {
+				else if (y == 0 && x == m.game.state.gardenSize - 1) {
 		            m.game.layers.borders.putTileAt(2, x, y)
 				}
 				
 				//Bottom left corner
-				else if (y == maxPosY - 1 && x == m.game.state.gardenPos.x) {
+				else if (y == m.game.state.gardenSize - 1 && x == 0) {
 		            m.game.layers.borders.putTileAt(6, x, y)
 				}
 				
 				//Bottom right corner
-				else if (y == maxPosY - 1 && x == maxPosX-1) {
+				else if (y == m.game.state.gardenSize - 1 && x == m.game.state.gardenSize - 1) {
 		            m.game.layers.borders.putTileAt(8, x, y)
-					m.game.layers.grid.putTileAt(4, x, y)
 				}
 				
 				//Normal grid cells 
 				else {
 					
-					//Add the remaining grid bits
-					if (x == maxPosX - 1 || y == maxPosY - 1) {
+					//TODO: make middle bottom grid
+					
+					if (x == m.game.state.gardenSize - 1 ) {
+						m.game.layers.grid.putTileAt(11, x, y)
+					}
+					else if (y == m.game.state.gardenSize - 1) {
 						m.game.layers.grid.putTileAt(10, x, y)
 					}
+				
 				}
 			}
 			
@@ -183,13 +205,16 @@ angular.module('farma', [])
 	
 	//Hands back an array of x-y notations of valid cells
 	m.returnValidFarmCells = (X, Y, maxX, maxY) => {
+
+		
 		let validCells = []
-		for (let y = Y; y < maxY; y++) {
-			for (let x = X; x < maxX; x++) {
+		for (let y = 0; y < m.game.state.gardenSize; y++) {
+			for (let x = 0; x < m.game.state.gardenSize; x++) {
 				validCells.push(m.toPosStr(x, y))
 			}
 		}
 		
+		m.game.validCells = validCells
 		return validCells
 	}
 	
@@ -217,11 +242,12 @@ angular.module('farma', [])
 			cell = m.game.state.plants[posStr]
 		
 		//Check if the cell is valid (aka empty)
-		if ((!cell || cell.isSoil) || force) {
+		if ((!cell || (!!cell && cell.isSoil)) || force) {
 			//Set the cell to the new plant in the state
 			m.game.state.plants[posStr] = {
 				plantType: plantType,
-				soilType: (cell.isSoil ? cell.soilType || 'normal')
+				soilType: (cell && cell.isSoil ? cell.soilType : 'normal'),
+				fertilizerType: (cell && cell.isSoil ? cell.fertilizerType : 'normal'),
 				tile: m.game.layers.crops.putTileAt(plantType, x, y),
 				timestamp: new Date().getTime()
 			}
@@ -248,7 +274,7 @@ angular.module('farma', [])
 		
 		//Set the width and height
 		m.game.config.width = ((16 + 1) * m.game.config.tileSize)
-		m.game.config.height = ((10 + 1) * m.game.config.tileSize)
+		m.game.config.height = ((16 + 1) * m.game.config.tileSize)
 		
 		//Create the engine
 		m.game.engine = new Phaser.Game(m.game.config)
@@ -274,7 +300,14 @@ angular.module('farma', [])
 	//Handle creating some objects, after they've loaded
 	m.engineCreate = (t) => {
 		//Make the map
-	    m.game.map = t.make.tilemap({ width: m.game.config.width, height: m.game.config.height, tileWidth: m.game.config.tileSize, tileHeight: m.game.config.tileSize })
+	    m.game.map = t.make.tilemap({
+			x: 0,
+			y: 0,
+			width: 32,
+			height: 32,
+			tileWidth: m.game.config.tileSize,
+			tileHeight: m.game.config.tileSize
+	    })
 		
 
 		//Register the tilesets
@@ -286,8 +319,8 @@ angular.module('farma', [])
 		
 		m.game.canvas.on('mousemove', (e) => {
 			let offset = m.game.canvas.offset(),
-				x = e.clientX - offset.left,
-				y = e.clientY - offset.top,
+				x = (e.clientX - offset.left) - m.game.cameraPos.x,
+				y = (e.clientY - offset.top) - m.game.cameraPos.y,
 				
 				// Rounds down to nearest tile
 				pointerTileX = m.game.map.worldToTileX(x),
@@ -308,44 +341,57 @@ angular.module('farma', [])
 		//Register the layers
 		m.game.layers = {}
 		
+		m.game.layers.soils = m.game.map.createBlankDynamicLayer('soils', m.game.tiles.soils),
+		m.game.layers.crops = m.game.map.createBlankDynamicLayer('crops', m.game.tiles.crops)
+		
+		//Create the valid cells, and garden bed
+		m.returnValidFarmCells()
+		
 		//Create the borders
 		m.createBorders()
 		
+		//Calculate the center position of the garden for the camera
+		m.game.cameraPos = {
+	    	x: (m.game.config.width/2) - ((m.game.state.gardenSize/2) * m.game.config.tileSize),
+			y: (m.game.config.height/2) - ((m.game.state.gardenSize/2 * m.game.config.tileSize))
+		}
 		
-		m.game.layers.soils = m.game.map.createBlankDynamicLayer('soils', m.game.tiles.soils),
-		m.game.layers.crops = m.game.map.createBlankDynamicLayer('crops', m.game.tiles.crops)
+		//Set the camera position
+		t.cameras.main.setPosition(m.game.cameraPos.x, m.game.cameraPos.y)
 		
 	}
 		
 	//The main game loop, should probably just use this to call other loop funcs... dunno
 	m.engineUpdate = (t) => {
+		m.game._t = t
 		
-		//All the mouse stuff
-		if (m.game.mouse) {
-			
-			//Cell checks
-			let posStr = m.toPosStr(m.game.mouse.tileX, m.game.mouse.tileY),
-				validCell = m.game.validCells.indexOf(posStr) > -1
-			
-			//Was the mouse clicked?
-			if (m.game.mouse.clicked) {
+		if (m.game.map) {
+			//All the mouse stuff
+			if (m.game.mouse) {
+				//Cell checks
+				let posStr = m.toPosStr(m.game.mouse.tileX, m.game.mouse.tileY),
+					validCell = m.game.validCells.indexOf(posStr) > -1
 				
-				//Is this a valid location?
-				if (validCell) {				
-					m.sowPlant(32, m.game.mouse.tileX, m.game.mouse.tileY)
+				//Was the mouse clicked?
+				if (m.game.mouse.clicked) {
+					
+					//Is this a valid location?
+					if (validCell) {				
+						m.sowPlant(32, m.game.mouse.tileX, m.game.mouse.tileY)
+					}
+					
+					//Clear the clicked state
+					m.game.mouse.clicked = false
 				}
 				
-				//Clear the clicked state
-				m.game.mouse.clicked = false
-			}
+				if (validCell) {
+					m.game.engine.scene.scenes[0].input.setDefaultCursor('pointer');
+				}
+				else {
+					m.game.engine.scene.scenes[0].input.setDefaultCursor('default');
+				}
 			
-			if (validCell) {
-				m.game.engine.scene.scenes[0].input.setDefaultCursor('pointer');
 			}
-			else {
-				m.game.engine.scene.scenes[0].input.setDefaultCursor('default');
-			}
-		
 		}
 
 	}
