20594c7a3b9342a41066d6ba34e1bae48a668eab
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2020-11-24 12:09:49 +1000

    Grid working, planting getting there, lots to do still

--- sys/js/main.js
+++ sys/js/main.js
@@ -113,21 +113,40 @@ angular.module('farma', [])
 		//TODO: Use this to get all permanent serializable items - usually for saving
 	}
 	
+	//Creates the grids and borders for the planting patch
 	m.createBorders = () => {
 		
-		//Destroy the 
+		//Destroy the layers (if they exist), then remake them 
+		if (m.game.layers.grid) m.game.layers.grid.destroy()
+		m.game.layers.grid = m.game.map.createBlankDynamicLayer('grid', m.game.tiles.borders)
+		
 		if (m.game.layers.borders) m.game.layers.borders.destroy()
 		m.game.layers.borders = m.game.map.createBlankDynamicLayer('borders', m.game.tiles.borders)
 		
-		//Set the top left of the garden bed
+		//Set the top left of the garden bed, and save that to the game object
 		m.game.state.gardenPos = m.game.map.worldToTileXY((m.game.config.width / 2) - (m.game.state.gardenSize / 2), (m.game.config.height / 2) - (m.game.state.gardenSize / 2))
-		m.game.state.gardenPos. x = m.game.state.gardenPos.x - 1
-		m.game.state.gardenPos. y = m.game.state.gardenPos.y - 1
-		
+		m.game.state.gardenPos.x = m.game.state.gardenPos.x - 1
+		m.game.state.gardenPos.y = m.game.state.gardenPos.y - 1
 		let maxPosX = m.game.state.gardenPos.x + m.game.state.gardenSize,
 			maxPosY = m.game.state.gardenPos.y + m.game.state.gardenSize
+		
+		
+		
+		
+		
+		m.game.validCells = m.returnValidFarmCells(m.game.state.gardenPos.x, m.game.state.gardenPos.y, maxPosX, maxPosY)
+		
 		for (let y = m.game.state.gardenPos.y; y < maxPosY; y++) {
 			for (let x = m.game.state.gardenPos.x; x < maxPosX; x++) {
+				
+
+				
+				
+				//Add the top left corner grid tile
+				m.game.layers.grid.putTileAt(9, x, y)
+
+				//Handle the corners
+				
 				//Top left corner
 				if (y == m.game.state.gardenPos.y && x == m.game.state.gardenPos.x) {
 		            m.game.layers.borders.putTileAt(0, x, y)
@@ -138,16 +157,75 @@ angular.module('farma', [])
 		            m.game.layers.borders.putTileAt(2, x, y)
 				}
 				
-				//bottom left corner
-				if (y == maxPosY - 1 && x == m.game.state.gardenPos.x) {
+				//Bottom left corner
+				else if (y == maxPosY - 1 && x == m.game.state.gardenPos.x) {
 		            m.game.layers.borders.putTileAt(6, x, y)
 				}
 				
-				//bottom right corner
+				//Bottom right corner
 				else if (y == maxPosY - 1 && x == maxPosX-1) {
 		            m.game.layers.borders.putTileAt(8, x, y)
+					m.game.layers.grid.putTileAt(4, x, y)
+				}
+				
+				//Normal grid cells 
+				else {
+					
+					//Add the remaining grid bits
+					if (x == maxPosX - 1 || y == maxPosY - 1) {
+						m.game.layers.grid.putTileAt(10, x, y)
+					}
 				}
 			}
+			
+		}
+	}
+	
+	//Hands back an array of x-y notations of valid cells
+	m.returnValidFarmCells = (X, Y, maxX, maxY) => {
+		let validCells = []
+		for (let y = Y; y < maxY; y++) {
+			for (let x = X; x < maxX; x++) {
+				validCells.push(m.toPosStr(x, y))
+			}
+		}
+		
+		return validCells
+	}
+	
+	//return the name string for a position
+	m.toPosStr = (x, y) => {
+		return `(${x}-${y})`
+	}
+	
+	//Return the x and y for a name string position
+	m.fromPosStr = (str) => {
+		let raw = str.substr(1, str.length - 2).split('-'),
+			pos = {
+				x: parseInt(raw[0]),
+				y: parseInt(raw[1]),
+			}
+		
+		return pos
+	}
+	
+	//Plants a plant in a cell, if the cell is free / forced
+	m.sowPlant = (plantType, x, y, force) => {
+		
+		//Get the position string, and the cell
+		let posStr = m.toPosStr(x, y),
+			cell = m.game.state.plants[posStr]
+		
+		//Check if the cell is valid (aka empty)
+		if ((!cell || cell.isSoil) || force) {
+			//Set the cell to the new plant in the state
+			m.game.state.plants[posStr] = {
+				plantType: plantType,
+				soilType: (cell.isSoil ? cell.soilType || 'normal')
+				tile: m.game.layers.crops.putTileAt(plantType, x, y),
+				timestamp: new Date().getTime()
+			}
+			
 			
 		}
 	}
@@ -187,6 +265,7 @@ angular.module('farma', [])
 	
 	//Handles preloading assets
 	m.enginePreload = (t) => {
+		t.load.image('grid', 'sys/img/grid-cell.png');
 		//t.load.image('soils', 'sys/img/soils.png');
 		t.load.image('crops', 'sys/img/crops.png');
 		t.load.image('borders', 'sys/img/border-tiles.png');
@@ -197,14 +276,13 @@ angular.module('farma', [])
 		//Make the map
 	    m.game.map = t.make.tilemap({ width: m.game.config.width, height: m.game.config.height, tileWidth: m.game.config.tileSize, tileHeight: m.game.config.tileSize })
 		
+
 		//Register the tilesets
 		m.game.tiles = {
 			borders: m.game.map.addTilesetImage('borders', 'borders', 32, 32),
 			soils: m.game.map.addTilesetImage('soils', 'soils', 32, 32),
 			crops: m.game.map.addTilesetImage('crops', 'crops', 32, 32)
 		}
-		
-		
 		
 		m.game.canvas.on('mousemove', (e) => {
 			let offset = m.game.canvas.offset(),
@@ -233,6 +311,7 @@ angular.module('farma', [])
 		//Create the borders
 		m.createBorders()
 		
+		
 		m.game.layers.soils = m.game.map.createBlankDynamicLayer('soils', m.game.tiles.soils),
 		m.game.layers.crops = m.game.map.createBlankDynamicLayer('crops', m.game.tiles.crops)
 		
@@ -241,13 +320,32 @@ angular.module('farma', [])
 	//The main game loop, should probably just use this to call other loop funcs... dunno
 	m.engineUpdate = (t) => {
 		
+		//All the mouse stuff
+		if (m.game.mouse) {
+			
+			//Cell checks
+			let posStr = m.toPosStr(m.game.mouse.tileX, m.game.mouse.tileY),
+				validCell = m.game.validCells.indexOf(posStr) > -1
+			
+			//Was the mouse clicked?
+			if (m.game.mouse.clicked) {
+				
+				//Is this a valid location?
+				if (validCell) {				
+					m.sowPlant(32, m.game.mouse.tileX, m.game.mouse.tileY)
+				}
+				
+				//Clear the clicked state
+				m.game.mouse.clicked = false
+			}
+			
+			if (validCell) {
+				m.game.engine.scene.scenes[0].input.setDefaultCursor('pointer');
+			}
+			else {
+				m.game.engine.scene.scenes[0].input.setDefaultCursor('default');
+			}
 		
-		if (m.game.mouse && m.game.mouse.clicked) {
-			console.log(m.game.mouse.tileX, m.game.mouse.tileY)
-			
-			m.game.layers.crops.putTileAt(0, m.game.mouse.tileX, m.game.mouse.tileY)
-			
-			m.game.mouse.clicked = false
 		}
 
 	}
